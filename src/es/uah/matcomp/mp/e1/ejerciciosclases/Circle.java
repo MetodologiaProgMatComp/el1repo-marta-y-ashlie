8d75027c81c1050353f62f69eeffd81a84525c61
AshlieSalcedo <157625748+AshlieSalcedo@users.noreply.github.com>
2024-02-10 15:06:53 +0100

    Circle
    Se ha añadido la clase Circle y TestMain

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/Circle.java
@@ -0,0 +1,30 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+/**
+ * La clase Circle moldea un circulo con un radio
+ */
+public class Circle {
+    private double radius;
+    public Circle(){
+        radius = 1.0;
+
+    }
+    public Circle(double r){
+        radius = r;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+    public double getCircumference() {
+        return 2*Math.PI*radius;
+    }
+    public String toString() {
+        return "Circle[radius= " + radius + "]";
+    }
+    public void setRadius(double newRadius){
+        radius = newRadius;
+    }
+
+}
