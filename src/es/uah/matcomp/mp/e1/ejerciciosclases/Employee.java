ff0f8097b3d5d47651da18438efc91eff6f75adf
AshlieSalcedo <157625748+AshlieSalcedo@users.noreply.github.com>
2024-02-10 17:01:54 +0100

    Clase Employee
    Se ha añadido la clase Employee

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/Employee.java
@@ -0,0 +1,43 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    public Employee(int i, String fN, String lN, int sy){
+        id=i;
+        firstName= fN;
+        lastName=lN;
+        salary=sy;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getName(){
+        return firstName + lastName;
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public int getAnnualSalary(){
+        return salary * 12;
+    }
+    public void setSalary( int newSalary){
+        salary = newSalary;
+    }
+    public int raiseSalary( int percent){
+        return salary * (percent/100);
+    }
+    public String toString(){
+        return "Employee[id= " + id + ", name = " + firstName +" " + lastName + ", salary = " + salary + "]";
+    }
+
+
+}
