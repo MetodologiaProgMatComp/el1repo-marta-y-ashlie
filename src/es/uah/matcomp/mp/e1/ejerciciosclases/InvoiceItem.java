e0f452765f1ddb93a98b835b508f5a3f1908d608
AshlieSalcedo <157625748+AshlieSalcedo@users.noreply.github.com>
2024-02-10 17:23:00 +0100

    Clase InvoiceItem
    Se ha añadido la clase InvoiceItem y su clase test

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/InvoiceItem.java
@@ -0,0 +1,41 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class InvoiceItem {
+    private String id;
+    private String desc;
+    private int qty;
+    private double unitPrice;
+
+    public InvoiceItem(String idU,String descU, int qtyU, double unitPriceU){
+        id=idU;
+        desc=descU;
+        qty=qtyU;
+        unitPrice=unitPriceU;
+
+    }
+    public String getId(){
+        return id;
+    }
+    public String getDesc(){
+        return desc;
+    }
+    public int getQty(){
+        return qty;
+    }
+    public double getUnitPrice(){
+        return unitPrice;
+    }
+    public double getTotal(){
+        return unitPrice * qty;
+    }
+    public void setQty( int newQty){
+        qty = newQty;
+
+    }
+    public void setUnitPrice(double newUnitPrice){
+        unitPrice= newUnitPrice;
+    }
+    public String toString(){
+        return "InvoiceItem[id= "+ id + "desc = " + desc + ", qty = " + qty + ", unitPrice = " + unitPrice + "]";
+    }
+}
