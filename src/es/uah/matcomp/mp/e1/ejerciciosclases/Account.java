be2f42524508ac9398b540efebdf4758514e110b
martita49 <martita.llamazares@gmail.com>
2024-02-15 17:24:38 +0100

    Ejercicios 6 y 9

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/Account.java
@@ -0,0 +1,61 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class Account {
+
+    private String id;
+
+    private String name;
+
+    private int balance = 0;
+
+    public  Account(String ID, String NAME) {
+        id=ID;
+        name=NAME;
+    }
+    public Account(String ID, String NAME, int Balance) {
+
+        id=ID;
+        name=NAME;
+        balance=Balance;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int credit(int amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public int debit(int amount) {
+        if (amount <= balance) {
+            balance-= amount;
+        } else{
+            System.out.println("Amount exceeded balance");
+        }
+        return balance;
+    }
+
+    public int transferTo(Account another, int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            another.balance += amount;
+        } else {
+            System.out.println("Amount exceeded balance");
+        }
+        return balance;
+    }
+
+    public String toString() {
+        return "Account[id="+  id + ", name=" + name + ", balance=" + balance + "]";
+    }
+}
