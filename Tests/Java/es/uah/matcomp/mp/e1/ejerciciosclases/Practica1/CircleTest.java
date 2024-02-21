2b14966c82eea40cfde5b8e931d9d1ec5bbb5980
AshlieSalcedo <157625748+AshlieSalcedo@users.noreply.github.com>
2024-02-21 13:00:02 +0100

    test circle

---
+++ Tests/Java/es/uah/matcomp/mp/e1/ejerciciosclases/Practica1/CircleTest.java
@@ -0,0 +1,42 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.Practica1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CircleTest {
+
+    @org.junit.jupiter.api.Test
+    void getRadius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getArea() {
+        double radius = 3.0;
+        Circle circle = new Circle(radius);
+        double areaEsperada = Math.PI * radius * radius;
+        assertEquals(areaEsperada, circle.getArea());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCircumference() {
+        double radius = 2.0;
+        Circle circle = new Circle(radius);
+        double circumferenciaEsperada = 2 * Math.PI * radius;
+        assertEquals(circumferenciaEsperada,circle.getCircumference());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+        double radius = 3.0;
+        Circle circle = new Circle(radius);
+        assertEquals("Circle[radius = 3]", circle.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRadius() {
+        Circle circle = new Circle();
+        double nuevoRadius = 2.9;
+        circle.setRadius(nuevoRadius);
+        assertEquals(nuevoRadius, circle.getRadius());
+    }
+}
