ee579d81cada72cea89777a140c60b351f45a193
martita49 <martita.llamazares@gmail.com>
2024-02-21 12:29:06 +0100

    Borramos la carpeta de test y creamos la que hay que usar

--- Tests/Java/es/uah/matcomp/mp/e1/ejerciciosclases/Practica1/AccountTest.java
+++ Tests/Java/es/uah/matcomp/mp/e1/ejerciciosclases/Practica1/AccountTest.java
@@ -6,14 +6,21 @@ class AccountTest {
 
     @org.junit.jupiter.api.Test
     void getID() {
+        Account Pepe = new Account("223", "Pepe");
+        assertEquals("223",Pepe.getID());
     }
 
     @org.junit.jupiter.api.Test
     void getName() {
+        Account Pepa = new Account("2", "Pepa");
+        assertEquals("Pepa", Pepa.getName());
     }
 
     @org.junit.jupiter.api.Test
     void getBalance() {
+        int balance = 1000;
+        Account Pepo = new Account("2232","Pepo",balance);
+        assertEquals();
     }
 
     @org.junit.jupiter.api.Test
