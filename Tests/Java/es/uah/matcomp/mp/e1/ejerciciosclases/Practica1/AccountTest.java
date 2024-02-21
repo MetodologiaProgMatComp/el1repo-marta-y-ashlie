82ceefcbeaf93cbfe579cd456595bf4db9a653d7
martita49 <martita.llamazares@gmail.com>
2024-02-21 13:03:35 +0100

    Borramos la carpeta de test y creamos la que hay que usar

--- Tests/Java/es/uah/matcomp/mp/e1/ejerciciosclases/Practica1/AccountTest.java
+++ Tests/Java/es/uah/matcomp/mp/e1/ejerciciosclases/Practica1/AccountTest.java
@@ -20,15 +20,43 @@ class AccountTest {
     void getBalance() {
         int balance = 1000;
         Account Pepo = new Account("2232","Pepo",balance);
-        assertEquals();
+        assertEquals(balance,Pepo.getBalance());
     }
 
     @org.junit.jupiter.api.Test
     void credit() {
+        int amount = 1000;
+        Account Pepi = new Account("212", "Pepi");
+
+        assertEquals();
     }
 
     @org.junit.jupiter.api.Test
     void debit() {
+        public void testDebit() {
+            Account account = new Account("123456", "John Doe", 1000);
+
+            assertEquals(1000, account.getBalance());
+
+            account.debit(500);
+            assertEquals(500, account.getBalance());
+
+            account.debit(500);
+            assertEquals(0, account.getBalance());
+
+            account.debit(500);
+            assertEquals(0, account.getBalance());
+        }
+
+        @Test
+        public void testDebitExceededBalance() {
+            Account account = new Account("123456", "John Doe", 1000);
+
+            account.debit(1500);
+
+            assertEquals(1000, account.getBalance());
+        }
+        }
     }
 
     @org.junit.jupiter.api.Test
